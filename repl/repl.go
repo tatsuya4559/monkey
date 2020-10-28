b90b7151e5b8a2dfadb73a66c7edeaa510b40915
tatsuya4559 <tatsuya.k1029@gmail.com>
2020-10-29 08:19:27 +0900

    Connect parser to repl

--- repl/repl.go
+++ repl/repl.go
@@ -6,7 +6,7 @@ import (
 	"io"
 
 	"github.com/tatsuya4559/monkey/lexer"
-	"github.com/tatsuya4559/monkey/token"
+	"github.com/tatsuya4559/monkey/parser"
 )
 
 const PROMPT = ">> "
@@ -23,9 +23,21 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParseErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParseErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }

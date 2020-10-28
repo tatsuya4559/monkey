3e0e7254e254dd76a1f47f942d5ea63afa3f3e74
tatsuya4559 <tatsuya.k1029@gmail.com>
2020-10-29 08:25:27 +0900

    Pretty error msg

--- repl/repl.go
+++ repl/repl.go
@@ -10,6 +10,13 @@ import (
 )
 
 const PROMPT = ">> "
+const MONKEY_FACE = `
+　　　　／三ヽ
+　　　 /(( ‥|)
+　　 ／　( ┴)
+　～(　|| ノ||
+　　(_(∪)_)∪
+`
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
@@ -37,6 +44,9 @@ func Start(in io.Reader, out io.Writer) {
 }
 
 func printParseErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
 	for _, msg := range errors {
 		io.WriteString(out, "\t"+msg+"\n")
 	}

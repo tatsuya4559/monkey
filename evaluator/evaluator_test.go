1028176e95d79635baba544c1c69687bd131e378
tatsuya4559 <tatsuya.k1029@gmail.com>
2020-11-03 17:59:42 +0900

    Add builtin function: len

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -349,3 +349,35 @@ let addTwo = newAdder(2);
 addTwo(5);`
 	testIntegerObject(t, testEval(input), 7)
 }
+
+func TestBuiltinFunctions(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{`len("")`, 0},
+		{`len("four")`, 4},
+		{`len("hello world")`, 11},
+		{`len(1)`, "argument to `len` not supported, got INTEGER"},
+		{`len("one", "two")`, "wrong number of arguments. want=1, got=2"},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+
+		switch expected := tt.expected.(type) {
+		case int:
+			testIntegerObject(t, evaluated, int64(expected))
+		case string:
+			errObj, ok := evaluated.(*object.Error)
+			if !ok {
+				t.Fatalf("object is not Error. got=%T (%+v)",
+					evaluated, evaluated)
+			}
+			if errObj.Message != expected {
+				t.Errorf("wrong error message. want=%q, got=%q",
+					expected, errObj.Message)
+			}
+		}
+	}
+}

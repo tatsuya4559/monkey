c0b977e50cf7bf02a774ed6424ecde091eacf328
tatsuya4559 <tatsuya.k1029@gmail.com>
2020-11-04 22:24:05 +0900

    Extract function

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -3,22 +3,22 @@ package evaluator
 import "github.com/tatsuya4559/monkey/object"
 
 var builtins = map[string]*object.Builtin{
-	"len": {
-		Fn: func(args ...object.Object) object.Object {
-			if len(args) != 1 {
-				return newError("wrong number of arguments. want=1, got=%d",
-					len(args))
-			}
+	"len": {Fn: Len},
+}
 
-			switch arg := args[0].(type) {
-			case *object.String:
-				return &object.Integer{Value: int64(len(arg.Value))}
-			case *object.Array:
-				return &object.Integer{Value: int64(len(arg.Elements))}
-			default:
-				return newError("argument to `len` not supported, got %s",
-					arg.Type())
-			}
-		},
-	},
+func Len(args ...object.Object) object.Object {
+	if len(args) != 1 {
+		return newError("wrong number of arguments. want=1, got=%d",
+			len(args))
+	}
+
+	switch arg := args[0].(type) {
+	case *object.String:
+		return &object.Integer{Value: int64(len(arg.Value))}
+	case *object.Array:
+		return &object.Integer{Value: int64(len(arg.Elements))}
+	default:
+		return newError("argument to `len` not supported, got %s",
+			arg.Type())
+	}
 }

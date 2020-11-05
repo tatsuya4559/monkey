67badb40bd01406d70d84ff92e071ec3207df452
tatsuya4559 <tatsuya.k1029@gmail.com>
2020-11-05 22:01:38 +0900

    Eval hash key access

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -265,6 +265,14 @@ if (10 > 1) {
 			`"hello" - "world"`,
 			"unknown operator: STRING - STRING",
 		},
+		{
+			`{[1,2]: "Monkey"};`,
+			"unusable as hash key: ARRAY",
+		},
+		{
+			`{"name": "Monkey"}[fn(x){x}];`,
+			"unusable as hash key: FUNCTION",
+		},
 	}
 
 	for _, tt := range tests {
@@ -537,5 +545,52 @@ func TestHashLiterals(t *testing.T) {
 			t.Errorf("no pair for given key in Pairs")
 		}
 		testIntegerObject(t, pair.Value, expectedValue)
+	}
+}
+
+func TestHashIndexExpression(t *testing.T) {
+	tests := []struct {
+		input string
+		expected interface{}
+	}{
+		{
+			`{"foo": 5}["foo"]`,
+			5,
+		},
+		{
+			`{"foo": 5}["bar"]`,
+			nil,
+		},
+		{
+			`let key = "foo"; {"foo": 5}[key]`,
+			5,
+		},
+		{
+			`{}["foo"]`,
+			nil,
+		},
+		{
+			`{5: 5}[5]`,
+			5,
+		},
+		{
+			`{true: 5}[true]`,
+			5,
+		},
+		{
+			`{false: 5}[false]`,
+			5,
+		},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		switch expected := tt.expected.(type) {
+		case int:
+			testIntegerObject(t, evaluated, int64(expected))
+		default:
+			testNullObject(t, evaluated)
+		}
+
 	}
 }
